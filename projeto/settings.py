e8d19b554d87baa53d6bc0f614c0f7be758e8a97
EvertonMachado1 <everton1234ramosmachado@gmail.com>
2025-04-04 17:35:10 -0300

    banco do django migrado pro mysql

--- projeto/settings.py
+++ projeto/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pi_univesp',
+        'USER': 'pi',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
