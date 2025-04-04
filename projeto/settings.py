71429e9fedc490cd3739db4e182d2aeef7fc4e7e
EvertonMachado1 <everton1234ramosmachado@gmail.com>
2025-04-04 17:38:24 -0300

    adicionadas as pastas templetes, static, css, js, images e os path

--- projeto/settings.py
+++ projeto/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'projeto' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
