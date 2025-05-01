c1517b5b8d29e3a27cf02e064537a74b31058c70
EvertonMachado1 <everton1234ramosmachado@gmail.com>
2025-05-01 13:18:34 -0300

    models

--- core/views.py
+++ core/views.py
@@ -1,9 +1,10 @@
 from agenda import urls
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from core.models import agendamento_de_aula
 
 
 #render tela inicial
 @login_required
 def tela_principal(request):
-    return render(request, 'tela_principal.html')
+    return render(request, 'tela_principal.html', {'agendamentos': agendamento_de_aula})
