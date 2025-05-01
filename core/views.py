d6d3cf154a868d32d902cc8147248da3ded10bf0
EvertonMachado1 <everton1234ramosmachado@gmail.com>
2025-05-01 12:43:47 -0300

    login required

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from agenda import urls
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 #render tela inicial
+@login_required
 def tela_principal(request):
     return render(request, 'tela_principal.html')
